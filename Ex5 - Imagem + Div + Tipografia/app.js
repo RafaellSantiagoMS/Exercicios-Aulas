8562dc9066b10e2783f84faf55d109203cef6012
rafaell santiago <rafaellsantiagoms@gmail.com>
2025-05-13 22:36:52 -0300

    alo

--- Ex5 - Imagem + Div + Tipografia/app.js
+++ Ex5 - Imagem + Div + Tipografia/app.js
@@ -1,5 +1,17 @@
+ let alunos = ["Pedra","Papel", "Tesoura"]
 
-    for(i=0; 1<=10; i++){
-        console.log(i)
+ alunos.push = "Exodia"
 
-    }
+//  let numeroAleatorio = Math.floor(Math.random() * alunos.length)
+ console.log(alunos)
+
+ console.log(alunos.indexOf("Papel"))
+ console.log(alunos.includes("Exodia"))
+ alunos.forEach(function(pegaNomes, index){
+    console.log(pegaNomes.toLocaleUpperCase(),index)
+ }
+)
+
+const alunosModificado = alunos.map(function(pegaNomes){
+    console.log(pegaNomes + "lala" )
+})
